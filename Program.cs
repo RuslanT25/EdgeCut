0fec4e18f392a806f92fb85cffa2e5512e810e8c
Ruslan <tagizaderuslan25@gmail.com>
2023-11-23 14:47:39 +0400

    Add some admin panel codes

--- Program.cs
+++ Program.cs
@@ -34,6 +34,11 @@ namespace EdgeCut
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                  name: "Admin",
+                  pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+                );
+
+                app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
