a66c6c7d2de3ea0062bfb8e5b79c92d7a42b3c8d
Ruslan <tagizaderuslan25@gmail.com>
2023-11-23 16:12:35 +0400

    Add slider view to admin panel

--- Areas/Admin/Controllers/SliderController.cs
+++ Areas/Admin/Controllers/SliderController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EdgeCut.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class SliderController : Controller
     {
         // GET: SliderController
