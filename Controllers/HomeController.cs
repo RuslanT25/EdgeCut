4f022581d58f2989dff8d514d78f1e26dbe2cb3e
Ruslan <tagizaderuslan25@gmail.com>
2023-12-09 20:01:02 +0400

    Update Home Controller(get datas from admin panel)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EdgeCut.DAL;
+using EdgeCut.Models;
+using EdgeCut.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EdgeCut.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        readonly ApplicationContext _context;
+        public HomeController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
         {   
             ViewBag.Page = "Home";
-            return View();
+            List<Slider> sliders = await _context.Sliders.Where(x => x.DeletedAt == null).ToListAsync();
+            List<Blog> blogs = await _context.Blogs.Where(x => x.DeletedAt == null).ToListAsync();
+            List<Furniture> furnitures = await _context.Furnitures.Where(x => x.DeletedAt == null).ToListAsync();
+            List<Testimonial> testimonials = await _context.Testimonials.Where(x => x.DeletedAt == null).ToListAsync();
+
+            HomeViewModel homeViewModel = new()
+            {
+                Sliders = sliders,
+                Blogs = blogs,
+                Furnitures = furnitures,
+                Testimonials = testimonials
+            };
+
+            return View(homeViewModel);
         }
     }
 }
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EdgeCut.Models;
+
+namespace EdgeCut.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Slider> Sliders { get; set; }
+        public List<Blog> Blogs { get; set; }
+        public List<Furniture> Furnitures { get; set; }
+        public List<Testimonial> Testimonials { get; set; }
+    }
+}
